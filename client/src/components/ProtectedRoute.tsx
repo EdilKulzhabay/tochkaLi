f3ca44db721c066cb762c4333b62f3cc4491eae0
kulzhabai.e <kulzhabai.e@sailet.pro>
2025-12-03 21:59:51 +0500

    03.12.2025 1234

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 type Role = "user" | "admin" | "content_manager" | "client_manager" | "manager";
