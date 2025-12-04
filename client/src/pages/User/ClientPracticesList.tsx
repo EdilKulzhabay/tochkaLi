6dc437aa9b1ce6347e7f5c2d5e5a6977660450e0
kulzhabai.e <kulzhabai.e@sailet.pro>
2025-12-04 20:32:58 +0500

    04.12.2025 11

--- client/src/pages/User/ClientPracticesList.tsx
+++ client/src/pages/User/ClientPracticesList.tsx
@@ -239,7 +239,7 @@ export const ClientPracticesList = () => {
                                             ? {
                                                 height: `calc(${cardHeight}px + 0px)`,
                                                 ...(window.innerWidth >= 640 && window.innerWidth < 1024 ? { height: `calc(${cardHeight}px + 50px)` } : {}),
-                                                ...(window.innerWidth >= 1024 ? { height: `calc(${cardHeight}px + 50px)` } : {}),
+                                                ...(window.innerWidth >= 1024 ? { height: `calc(${cardHeight}px - 100px)` } : {}),
                                             }
                                             : {}
                                     }
--- client/src/pages/User/ClientVideoLessonsList.tsx
+++ client/src/pages/User/ClientVideoLessonsList.tsx
@@ -238,7 +238,7 @@ export const ClientVideoLessonsList = () => {
                                             ? {
                                                 height: `calc(${cardHeight}px + 0px)`,
                                                 ...(window.innerWidth >= 640 && window.innerWidth < 1024 ? { height: `calc(${cardHeight}px + 50px)` } : {}),
-                                                ...(window.innerWidth >= 1024 ? { height: `calc(${cardHeight}px + 50px)` } : {}),
+                                                ...(window.innerWidth >= 1024 ? { height: `calc(${cardHeight}px - 100px)` } : {}),
                                             }
                                             : {}
                                     }
