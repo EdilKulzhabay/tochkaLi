fd787bf6034cd4c3354676ffc8760e3d0073724c
kulzhabai.e <kulzhabai.e@sailet.pro>
2025-12-06 19:52:46 +0500

    06.12.2025 7

--- client/src/pages/User/BlockedUser.tsx
+++ client/src/pages/User/BlockedUser.tsx
@@ -1,5 +1,5 @@
 import bgGar from '../../assets/bgGar.png';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { RedButton } from '../../components/User/RedButton';
 
