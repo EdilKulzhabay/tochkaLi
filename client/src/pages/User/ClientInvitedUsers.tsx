c88761df16528e84183cd2e65ecb190a48baf703
kulzhabai.e <kulzhabai.e@sailet.pro>
2026-01-28 17:40:15 +0500

    28.01.2026 4

--- client/src/pages/User/ClientInvitedUsers.tsx
+++ client/src/pages/User/ClientInvitedUsers.tsx
@@ -283,7 +283,7 @@ export const ClientInvitedUsers = () => {
                                 <div className="flex flex-col gap-y-2">
                                     {invitedUsers.map((user: any) => (
                                         <a
-                                            href={`${user.telegramId ? `https://t.me/user?id=${user.telegramId}` : ''}`}
+                                            href={`${user.telegramUserName ? `https://t.me/${user.telegramUserName}` : ''}`}
                                             target="_blank"
                                             key={user.telegramId}
                                             className="block w-full py-2.5 px-3 border border-white/40 rounded-xl text-left text-white font-medium hover:bg-white/10 transition-colors"
